cad328ff74d6fb5699779e6939091d64aecc2920
Alexey <alexey.u@144mln.com>
2015-01-22 12:52:13 +0500

    Settings model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121063451) do
+ActiveRecord::Schema.define(version: 20150122075011) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20150121063451) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
+  end
+
+  create_table "settings", force: true do |t|
+    t.string   "name"
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
